6d860845f27f132c5eb8594661e90db3df4b483b
Prakash Kumar <141754712+prakash-ydv@users.noreply.github.com>
2024-12-26 20:09:09 +0530

    Add title to task status indicator
    Red - Task Pending
    Blue - Task Completed

--- src/components/TaskBox.jsx
+++ src/components/TaskBox.jsx
@@ -2,6 +2,7 @@ import React,{useState} from 'react'
 
 function Task(Props) {
   const [isDone, setisDone] = useState(false)
+  const [titleOfDot, settitleOfDot] = useState("Task Pending")
 
   // function to manage pending and completed task number
   function handleMarkDone (){
@@ -11,6 +12,7 @@ function Task(Props) {
       alert("Task Already Done")
     }
     setisDone(true)
+    settitleOfDot("Task Completed")
     
   }
 
@@ -18,7 +20,7 @@ function Task(Props) {
 
   return (
     <div className='border-2 relative border-purple-900 flex p-2 px-4 flex-col items-start justify-around h-[210px] w-[200px] rounded-3xl shadow-2xl cursor-pointer text-white overflow-auto scrollbar-none '>
-      <span className={`h-3 w-3 rounded-full absolute top-4 right-4 ${
+      <span title={titleOfDot} className={`h-3 w-3 rounded-full absolute top-4 right-4 ${
     isDone ? "bg-green-500" : "bg-red-500"
   }`} ></span><h1 className='font-semibold text-xl'>{Props.Title}</h1>
       <p className={isDone ? "line-through text-zinc-200 ":""}>{Props.Desc}</p>
